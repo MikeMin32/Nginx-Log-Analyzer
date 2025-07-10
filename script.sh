99a1f9ab621bd008712c4750fac04475b8ec21df
Mike Miniailo <mike.min.32@gmail.com>
2025-07-10 23:36:53 +0300

    Calculate top5 ips

--- script.sh
+++ script.sh
@@ -3,8 +3,16 @@
 #cat nginx-access.log | awk '{print $1}' | sort | grep "95.70.160.44" | wc
 
 cat nginx-access.log | awk '{print $1}' | sort -n | uniq  > ips.log
+touch count.log
+> count.log
 
 while IFS= read -r ip; do
-	echo "`grep $ip nginx-access.log | wc | awk '{print $1}'` - $ip" 
+	grep $ip nginx-access.log | wc | awk -v ip="$ip" '{print $1, ip}' >> count.log 
+	#count=$(grep -c "$ip" nginx-access.log)
+	#echo "$count - $ip"
+	#cat count.log >> "$count - $ip"
        	#grep $ip nginx-access.log | wc | awk '{print $1}'
 done < ips.log
+
+sort -n count.log >> count.log
+tail -n 5 count.log
